cf96fe5edd084e47425982bce9332a0ffc9c2682
blairmorrow <54084280+blairmorrow@users.noreply.github.com>
2019-09-10 18:00:37 +1200

    Comments added

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 ﻿// Project: Cast Iron
 // Author: Blair Morrow
 
+// Declare variables for the header element of the page and the top offset pixels for the header
 let header = document.getElementById("stickyHeader");
 let sticky = header.offsetTop;
 
+// When scrolling the web page call this function
 window.onscroll = function () {
 
+    // If the y-axis offset is greater than the header top offset
     if (window.pageYOffset > sticky) {
 
+        // Fix the header to the top of the web page as the user scrolls
         header.classList.add("sticky");
 
     }
 
+    // Otherwise
     else {
 
+        // Make the header behave as it normally would
         header.classList.remove("sticky");
 
     }
